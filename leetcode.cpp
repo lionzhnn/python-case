86fa88814cb2db7d5a1a15bc2109767c6fdca45d
lionzhnn <37626634+lionzhnn@users.noreply.github.com>
2020-09-27 21:09:09 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -380,3 +380,396 @@ int stringInput() {
 	}
 	return 0;
 }
+//剑指 Offer 11. 旋转数组的最小数字
+	int minArray(vector<int>& numbers) {
+		int begin = 0, end = numbers.size() - 1, mid ;
+		while (begin < end) {
+			mid = begin+(end-begin) / 2;
+			if (numbers[mid] < numbers[end])
+				end = mid;
+			else if (numbers[mid] > numbers[end])
+				begin = mid + 1;
+			else
+				end -= 1;
+		}
+		return numbers[begin];
+	}
+	//剑指 Offer 10- I. 斐波那契数列
+	int fib_one(int n) {
+		vector<int> fib_n{0,1};
+		for (int i = 2; i <= n; i++) {
+			fib_n.push_back(fib_n[i - 1]+fib_n[i - 2]);
+		}
+		return fib_n[n];
+	}
+	int fib_two(int n) {
+		if (n <= 1)
+			return n;
+		int index = 2, one = 0, two = 1, result;
+		while (index <= n) {
+			result = (one + two) % 1000000007;
+			one = two;
+			two = result;
+			index++;
+		}
+		return result;
+	}
+	//剑指 Offer 10- II. 青蛙跳台阶问题
+	int numWays(int n) {
+		if (n <= 1)
+			return 1;
+		int index = 2, one = 1, two = 1, result;
+		while (index <= n) {
+			result = (one + two) % 1000000007;
+			one = two;
+			two = result;
+			index++;
+		}
+		return result;
+	}
+	//剑指 Offer 07. 重建二叉树 递归方法 
+	void TreeTraverse_level(TreeNode* root) {
+		if (!root)
+			return;
+		queue<TreeNode*> traverse;
+		traverse.push(root);
+		while (!traverse.empty()) {
+			TreeNode* first = traverse.front();
+			traverse.pop();
+			cout << first->val << " ";
+			if (first->left)
+				traverse.push(first->left);
+			if (first->right)
+				traverse.push(first->right);
+		}
+		cout << endl;
+	}
+	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+		if (preorder.empty())
+			return NULL;
+		int root_value = preorder[0];
+		vector<int>::iterator index = inorder.begin();
+		while (*index != root_value)
+			index++;
+		vector<int> r_inorder,r_preorder,l_inorder,l_preorder;
+		//左子树中序
+		copy(inorder.begin(), index , back_inserter(l_inorder));
+		//右子树中序
+		copy(index + 1, inorder.end() , back_inserter(r_inorder));
+		//左子树前序
+		copy(preorder.begin()+1, preorder.begin()+l_inorder.size()+1, back_inserter(l_preorder));
+		//右子树前序
+		copy(preorder.begin() + l_inorder.size()+1, preorder.end() , back_inserter(r_preorder));
+		TreeNode* root = new TreeNode(*index);
+		root->left = buildTree(l_preorder, l_inorder);
+		root->right = buildTree(r_preorder, r_inorder);
+		return root;
+	}
+	//使用map优化拷贝，减少运行时间
+	TreeNode* buildTree_second(vector<int>& preorder, vector<int>& inorder) {
+		if (preorder.empty())
+			return nullptr;
+		map<int, int> in_order_map;
+		for (int i = 0; i < inorder.size(); i++) {
+			in_order_map[inorder[i]] = i;
+		}
+		TreeNode* root = buildTree_index(preorder, inorder, in_order_map, 0, preorder.size() - 1, 0, preorder.size() - 1);
+		return root;
+	}
+	TreeNode* buildTree_index(vector<int>& preorder, vector<int>& inorder,map<int,int> &in_order_map,int pre_begin,int pre_end,int in_begin,int in_end) {
+		//没有元素
+		if (pre_begin > pre_end)
+			return nullptr;
+		int root_value = preorder[pre_begin], root_index = in_order_map[root_value];
+		TreeNode* root = new TreeNode(root_value);
+		//只有一个元素
+		if (pre_begin == pre_end)
+			return root;
+		else {
+			root->left = buildTree_index(preorder, inorder, in_order_map, pre_begin+1, pre_begin-in_begin+root_index, in_begin, root_index - 1);
+			root->right = buildTree_index(preorder, inorder, in_order_map, pre_begin - in_begin + root_index + 1, pre_end, root_index + 1, in_end);
+		}
+		return root;
+	}
+	//剑指 Offer 06. 从尾到头打印链表
+	vector<int> reversePrint(ListNode* head) {
+		vector<int> result;
+		while (head) {
+			result.insert(result.begin(), head->val);
+		}
+		return result;
+	}
+	//剑指 Offer 05. 替换空格
+	string replaceSpace(string s) {
+		if (s.empty())
+			return "";
+		string temp;
+		for (char value : s) {
+			if (isspace(value))
+				temp += "%20";
+			else
+				temp += value;
+		}
+		return temp;
+	}
+	//剑指 Offer 04. 二维数组中的查找
+	bool findNumberInArray(vector<int> &matrix, int target) {
+		auto begin = matrix.begin();
+		auto end = matrix.end()-1;
+		while (end >= begin) {
+			auto mid = begin + (end - begin) / 2;
+			if (target < *begin)
+				return false;
+			if (target > *end)
+				return false;
+			if (target == *begin || target == *end || target == *mid)
+				return true;
+			if (target < *mid)
+				end = mid - 1;
+			else
+				begin = mid + 1;
+		}
+		return false;
+	}
+	bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+		if (matrix.empty())
+			return false;
+		for (vector<int> & value : matrix) {
+			if (value.empty())
+				return false;
+			if (findNumberInArray(value, target))
+				return true;
+		}
+		return false;
+	}
+	//剑指 Offer 03. 数组中重复的数字
+	int findRepeatNumber(vector<int>& nums) {
+		set<int> temp;
+		for (auto value : nums) {
+			if (temp.find(value) == temp.end()) {
+				temp.insert(value);
+			}
+			else {
+				return value;
+			}
+		}
+		return -1;
+
+	}
+	//剑指 Offer 12. 矩阵中的路径 回溯法
+	bool find_path(vector<vector<char>>& board, string &word, vector<vector<bool>>&flag, int word_index, int begin_x, int begin_y) {
+		if (begin_x < 0 || begin_x >= board.size() || begin_y < 0 || begin_y >= board[0].size())
+			return false;
+		if (word_index == word.size() - 1 && flag[begin_x][begin_y] && board[begin_x][begin_y] == word[word_index])
+			return true;
+		else if (word_index < word.size() && flag[begin_x][begin_y] && board[begin_x][begin_y] == word[word_index]) {
+			//剪枝已经找过的路径
+			flag[begin_x][begin_y] = false;
+			vector<vector<bool>> temp_flag = flag;
+			bool result = find_path(board, word, temp_flag, word_index + 1, begin_x, begin_y - 1)
+				|| find_path(board, word, temp_flag, word_index + 1, begin_x, begin_y + 1)
+				|| find_path(board, word, temp_flag, word_index + 1, begin_x - 1, begin_y)
+				|| find_path(board, word, temp_flag, word_index + 1, begin_x + 1, begin_y);
+			if (!result)
+				flag[begin_x][begin_y] = true;
+			return result;
+		}
+		//剪枝不满足的情况
+		return false;
+
+	}
+	bool exist(vector<vector<char>>& board, string &word) {
+		vector<pair<int, int>> begin_index;
+		vector<vector<bool>> flag, temp_flag;
+		for (int i = 0; i < board.size(); i++) {
+			vector<bool> temp;
+			for (int j = 0; j < board[0].size(); j++) {
+				temp.push_back(true);
+				//找到开始与单词首字母相同的下标，可能不止一个
+				if (board[i][j] == word[0])
+					begin_index.push_back(make_pair(i, j));
+			}
+			//初始标志位设为false
+			flag.push_back(temp);
+		}
+		for (int i = 0; i < begin_index.size(); i++) {
+			pair<int, int> temp_index = begin_index[i];
+			temp_flag = flag;
+			bool temp_result = find_path(board, word, temp_flag, 0, temp_index.first, temp_index.second);
+			if (temp_result)
+				return true;
+		}
+		return false;
+	}
+
+	//剑指 Offer 13. 机器人的运动范围
+	int get(int x) {
+		int result = 0;
+		while (x>0)
+		{
+			result = result + x % 10;
+			x = x / 10;
+		}
+		return result;
+	}
+	int moving(vector<vector<bool>> &mark, int m, int n, int k, int curR, int curC) {
+		if (curR < 0 || curR >= m || curC < 0 || curC >= n || !mark[curR][curC])
+			return 0;
+		else {
+			int curK = get(curR) + get(curC);
+			if (curK > k || curK < 0)
+				return 0;
+			mark[curR][curC] = false;
+			int result = 1+moving(mark,m, n, k,  curR, curC - 1) + moving(mark,m, n, k, curR, curC + 1)
+				+ moving(mark,m, n, k,  curR - 1, curC) + moving(mark,m, n, k, curR + 1, curC);
+			return result;
+		}
+	}
+	int movingCount(int m, int n, int k) {
+		const int array_m = m;
+		const int array_n = n;
+		vector<vector<bool>> mark;
+		for (int i = 0; i < m; i++) {
+			vector<bool> temp(n, true);
+			mark.push_back(temp);
+		}
+		return moving(mark, m, n, k, 0, 0);
+	}
+
+	//剑指 Offer 14- I. 剪绳子
+	//动态规划
+	int cuttingRope(int n) {
+		vector<vector<int>> dp;
+		for (int i = 0; i < n; i++) {
+			vector<int> temp;
+			if (i == 0) {
+				for (int j = 0; j < n; j++)
+					temp.push_back(j + 1);
+			}
+			else{
+				temp = vector<int>(n, 0);
+			}
+			dp.push_back(temp);
+		}
+		int result = 0;
+		for (int i = 1; i < n; i++) {
+			for (int j = i; j < 3; j++) {
+				int find_max = 0;
+				//数学证明，绳子长度为e时，得到的乘积最大，因为长度需要为整数，故取绳子长度尽量为3
+				for (int k = 1; k <= (j + 1) / 2 && k <= 3; k++) {
+					find_max = max(find_max, k*dp[i - 1][j-k]);
+				}
+				dp[i][j] = find_max;
+				result = (result >= dp[i][j] ? result : dp[i][j]);
+			}
+		}
+		return result;
+	}
+	//数学
+	int cuttingRope_two(int n) {
+		if (n <= 3)
+			return n - 1;
+		int a = n / 3, b = n % 3; 
+		if (b == 0)
+			return pow(a,1.0/3);
+		else if (b == 1)
+			return int(pow(a-1,1.0/3)) * 4;
+		else
+			return int(pow(a,1.0/3)) * 2;
+	}
+
+	//剑指 Offer 14- II. 剪绳子 II
+	int cuttingRope_II(int n) {
+		if (n <= 3) return n - 1;
+		if (n == 4) return 4;
+		long res = 1;
+		while (n > 4)
+		{
+			res *= 3;
+			res %= 1000000007;
+			n -= 3;
+		}
+		// 最后n的值只有可能是：2、3、4。而2、3、4能得到的最大乘积恰恰就是自身值
+		// 因为2、3不需要再剪了（剪了反而变小）；4剪成2x2是最大的，2x2恰巧等于4
+		return res * n % 1000000007;
+	}
+
+	//剑指 Offer 15. 二进制中1的个数
+	int hammingWeight(uint32_t n) {
+		int a, result = 0;
+		while (n>0) {
+			a = n % 2;
+			n = n / 2;
+			result = a ? result + 1 : result;
+		}
+		return result;
+	}
+
+	//剑指 Offer 16. 数值的整数次方
+	double power(double x, int n) {
+		if (n == 1)
+			return x;
+		double result = power(x, n / 2);
+		if (n % 2) {
+			result = result*result*x;
+		}
+		else
+			result = result*result;
+		return result;
+	}
+	double myPow(double x, int n) {
+		int a = abs(n);
+		bool b = n < 0 ? true : false;
+		if (a % 2)
+			return x*power(x, a - 1);
+		else
+			return power(x, a);
+	}
+
+	//剑指 Offer 17. 打印从1到最大的n位数
+	vector<int> printNumbers(int n) {
+		int x = 1;
+		int index = pow(10, n);
+		vector<int> result;
+		while (x < index) {
+			result.push_back(x);
+			x++;
+		}
+		return result;
+	}
+
+	//正则项匹配
+	bool isMatch(string s, string p) {
+		int m = s.size();
+		int n = p.size();
+		vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+		dp[0][0] = true;
+		for (int i = 0; i < n; i++) {
+			//如果p的第i+1个字符也就是p.charAt(i)是"*"的话，
+			//那么他就可以把p的第i个字符给消掉（也就是匹配0次）。
+			//我们只需要判断p的第i-1个字符和s的前0个字符是否匹
+			//配即可。比如p是"a*b*"，如果要判断p的第4个字符
+			//"*"和s的前0个字符是否匹配，因为字符"*"可以消去
+			//前面的任意字符，只需要判断p的"a*"和s的前0个字
+			//符是否匹配即可
+			if (p[i] == '*' && dp[0][i - 1]) {
+				dp[0][i + 1] = true;
+			}
+		}
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (p[j] == s[i] || p[j] == '.') {
+					dp[i + 1][j + 1] = dp[i][j];
+				}
+				else if (p[j] == '*') {
+					//递归公式
+					if (p[j-1] == s[i] || p[j-1] == '.') {
+						dp[i + 1][j + 1] = dp[i][j + 1];
+					}
+					dp[i + 1][j + 1] = dp[i + 1][j + 1] | dp[i + 1][j - 1];
+				}
+			}
+		}
+		return dp[m][n];
+}
+};
